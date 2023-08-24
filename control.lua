7f93fbdb72a79bc897a7b83caf2929dfd12c6bb9
sylvia <sylvia_lumen@proton.me>
2023-08-24 13:08:16 +0300

    add poison smoke trail

--- control.lua
+++ control.lua
@@ -4,15 +4,10 @@ script.on_event(defines.events.on_player_changed_position,
             if settings.global["isfire"].value then
                 player.surface.create_entity{name="fire-flame", position=player.position, force="neutral"};
             end
-        end
-)
-
-script.on_event(defines.events.on_player_changed_position,
-        function(event)
-            local player = game.get_player(event.player_index)
             if settings.global["issmoke"].value then
-                player.surface.create_entity{name="smoke", position=player.position, force="neutral"};
+                player.surface.create_entity{name="poison-cloud-visual-dummy", position=player.position, force="neutral"};
             end
+
         end
 )
 
